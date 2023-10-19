b9ee5683e8821dfcc25fbb6e0ee1891c76cde595
jpandya-test <j.panday1791@gmail.com>
2023-10-19 15:27:20 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! This is Jay Pandya'));
+app.get('/', (req, res) => res.send('Hello Team DevOps! This is Jay Pandya'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
